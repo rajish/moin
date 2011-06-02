6f24466b462379906ea4009035add1c4ab40afae
Rajish <rgaller@gazeta.pl>
2011-06-03 00:46:41 +0200

    Trying to deal with posting new item in the create invoice form, but still no effect - something is changing the POST request into GET.

--- app/views/Invoices/soldItem.js
+++ app/views/Invoices/soldItem.js
@@ -3,13 +3,21 @@
 		this.debug = true;
 		var form_fields = null;
 		
+		this.swap = function(content) {
+			this.$element.hide('slow').html(content).show('slow');
+		}
+		
 		this.get('#/', function(context) {
 			context.log('main');
 		});
 		
-		this.get('#/saveItem', function (context) {
-			context.log('saveItem');
+		this.post('#/saveItem', function (context) {
 			form_fields = this.params;
+			context.log('saveItem - form_fields = {');
+			for ( var item in this.params) {
+				context.log('item: ' + item);
+			}
+			context.log('}');
 			var action = #{jsAction @saveItem(':item') /};
 			this.partial(action({item: form_fields}));
 		});
