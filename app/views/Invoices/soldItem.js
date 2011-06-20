ffea22bbc32b11cf0099be4db046e913cd8d5314
Rajish <rgaller@gazeta.pl>
2011-06-21 00:09:47 +0200

    Working completion of valid entry selected in the new item area.

--- app/views/Invoices/soldItem.js
+++ app/views/Invoices/soldItem.js
@@ -2,7 +2,7 @@
 	var app = $.sammy('#invoiceItemsTable', function() {
 		this.debug = true;
 		this.raise_errors = true;
-		var form_fields = null;
+		var fields_values = null;
 		
 		this.swap = function(content) {
 			this.$element().hide('slow').html(content).show('slow');
@@ -10,6 +10,7 @@
 		
 		this.get('#/', function(context) {
 			context.log('main');
+			sammy = this;
 			$('#itemName').autocomplete({
 				source: function (request, response) {
 					Sammy.log("autocomplete::source req:" + request);
@@ -20,19 +21,26 @@
 					};
 					return context.send($.post, url(), data)
 						.then(function(contents) {
-							response($.map(contents.name, function(item) {
-								return {
-									label: item.name + item.description,
-									value: item.name
-								};	
-							}));
+							context.log("autocomplete contents: " + contents);
+							if (contents.length == 0) {
+								response([{label: "&{'inventory.item.unknown'}", value: null}]);
+							} else {
+								response($.map(contents, function(item) {
+									return {
+										label: item.name + " - " + item.description,
+										value: item
+									};	
+								}));
+							}
 						});
 				},
 				
 				minLength: 0,
 				
 				select: function (event, ui) {
-					Sammy.log("autocomplete::select");
+					fields_values = ui.item.value;
+					//event.stopPropagation();
+					//return false; // this cancels the default event action
 				},
 				
 				open: function () {
@@ -41,8 +49,27 @@
 				
 				close: function () {
 					Sammy.log("autocomplete::close");
+					if (fields_values != null) {
+						// TODO Handle the non-existing entry case
+						Sammy.log("autocomplete::close field_values: " + fields_values.toSource());
+						
+						$("input[name='item.item.name']").val(fields_values['name']);
+						$("textarea[name='item.item.description']").val(fields_values['description']);
+						$("input[name='item.retailPrice']").val(fields_values['price']);
+						$("input[name='item.quantity']").val("1");
+						$("input[name='item.vatRate']").val(fields_values['vatRate']);
+						sammy.trigger('recalc');
+						fields_values = null;
+					} else {
+						Sammy.log("fields_values are null");
+					}
 				}
 			});
+		});
+		
+		this.bind('recalc', function (){
+			// TODO recalculate values in the affected line
+			Sammy.log("event:recalc ");
 		});
 		
 		this.post('#/saveItem', function (context) {
@@ -56,13 +83,12 @@
 		
 		this.get('#/cancelEdits', function (context) {
 			context.log('cancelEdits');
+			('#itemsTable').reset();
 		});
 		
 	});
 	
 	$(function(	) {
-	
-	
 		app.run('#/');
 	});
 })(jQuery);
