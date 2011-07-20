915e24c766cc22cef213daa9444588122a0848d7
Rajish <rgaller@gazeta.pl>
2011-07-21 00:36:31 +0200

    Added JS locale number formatting and made some use of it

--- app/views/Invoices/soldItem.js
+++ app/views/Invoices/soldItem.js
@@ -89,7 +89,28 @@
 
 
     $(function( ) {
-        var viewModel = {
+    	Number.prototype.toMoney = function(decimals, decimal_sep, thousands_sep)
+    	{ 
+    		var n = this;
+    		//if decimal is zero we must take it, it means user does not want to show any decimal
+    		var c = isNaN(decimals) ? 2 : Math.abs(decimals);
+    		//if no decimal separator is passed we use the comma as default decimal separator (we MUST use a decimal separator)
+    		var d = decimal_sep || Number("1.2").toLocaleString().substr(1,1);  
+    		/*
+    		 * according to [http://stackoverflow.com/questions/411352/how-best-to-determine-if-an-argument-is-not-sent-to-the-javascript-function]
+    		 * the fastest way to check for not defined parameter is to use typeof value === 'undefined' 
+    		 * rather than doing value === undefined.
+    		 */
+    		//if you don't want to use a thousands separator you can pass empty string as thousands_sep value
+    		var t = (typeof thousands_sep === 'undefined') ? Number("1000").toLocaleString().substr(1,1) : thousands_sep; 
+    		var sign = (n < 0) ? '-' : '';
+    		//extracting the absolute value of the integer part of the number and converting to string
+    		var i = parseInt(n = Math.abs(n).toFixed(c)) + ''; 
+    		var j = ((j = i.length) > 3) ? j % 3 : 0; 
+    		return sign + (j ? i.substr(0, j) + t : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : ''); 
+    	}
+
+    	var viewModel = {
                 itemPrice: ko.observable(0),
                 itemDiscount: ko.observable(0),
                 itemQty: ko.observable(1),
@@ -100,13 +121,13 @@
         viewModel.itemNettVal = ko.dependentObservable(function () {
             var retval = parseFloat(viewModel.itemPrice()) * parseFloat(viewModel.itemQty()) * (1 - parseFloat(viewModel.itemDiscount())/100);
             Sammy.log("KO:itemNettVal recalculation: " + retval);
-            return retval;
+            return retval.toMoney();
         });
 
         viewModel.itemTotVal = ko.dependentObservable(function (){
             var retval = parseFloat(viewModel.itemNettVal()) * (1 + parseFloat(viewModel.itemVat())/100);
             Sammy.log("KO:itemTotVal recalculation: " + retval + " vat factor: " + (1 + parseFloat(viewModel.itemVat())/100));
-            return retval;
+            return retval.toMoney();
         });
 
         ko.applyBindings(viewModel);
